f973dcc1e65e8c8532c54b16a99a06af258ccec9
Ryan Fuller <145612331+ryanspage@users.noreply.github.com>
2023-10-25 12:37:26 -0500

    Revert "Separate & Organize Page Components"
    This reverts commit 561da1afb8e29c6771c15aeea4fa0024312ac75a.

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,20 @@
 (function($) {
-  "use strict"; // Start of use strict
+  /* Toggle Open & Close of Wiktionary Language Selection Dropdown */
+  $("#btn-wiktionary-lang-select").on('click', function (event) {
+    $("#dropdown-wiktionary-lang-select").toggleClass('show');
+  });
+
+  /* Close Wiktionary Language Selection Dropdown if Click Outside Dropdown */
+  $('body').on('click', function (e) {
+      const btnWiktionaryLangSelect = document.getElementById("btn-wiktionary-lang-select");
+      const wiktionaryDropdownLangSelect = document.getElementById("dropdown-wiktionary-lang-select");
+      if (btnWiktionaryLangSelect !== e.target &&
+          wiktionaryDropdownLangSelect !== e.target &&
+          !wiktionaryDropdownLangSelect.contains(e.target))
+      {
+        wiktionaryDropdownLangSelect.classList.remove('show');
+      }
+  });
 
   /* Countdown Timers */
   const romeDate = '/4/21';
@@ -76,8 +91,47 @@
       + '<div class="holder m-2"><span class="h3 font-weight-bold">%S</span> Sec</div>'));
   });
   /* End Countdown Timers */
+})(jQuery);
 
-})(jQuery); // End of use strict
+/* Wiktionary Search */
+function wiktionarySearch(searchBar, searchText) {
+  let wiktionaryURL = "https://en.wiktionary.org/wiki/";
+  let googleURL = "https://google.com/search?q=";
+  let searchURL = "";
+
+  const language = getWiktionaryLanguage(searchBar); //Get selected language
+
+  if (language == 0) { //English
+    searchURL = wiktionaryURL + searchText + "#English";
+  }
+  else if (language == 1) { //Latin
+    searchURL = wiktionaryURL + searchText + "#Latin";
+  }
+  else { //Ancient Greek
+    searchURL = googleURL + searchText + "+ancient+greek+wiktionary";
+  }
+
+  window.open(searchURL,'_blank');
+}
+
+/* Return Selected Language for Wiktionary Search*/
+function getWiktionaryLanguage(searchBar) {
+  let langs = [];
+
+  if (searchBar == 0) { //Desktop Topbar Search Box
+    langs = document.getElementsByName('langs1');
+  }
+  else { //Mobile Device Dropdown Search Box
+    langs = document.getElementsByName('langs2');
+  }
+
+  //Iterate through language radio buttons until active (selected) found
+  for (let i = 0, length = langs.length; i < length; i++) {
+    if (langs[i].classList.contains('active')) {
+      return i;
+    }
+  }
+}
 
 /* Get Countdown Year */
 function getCountdownYear(eventDate) {
--- js/load-components.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  $('#sidebar-container').load("components/sidebar.html");
-  $('#topbar-container').load("components/topbar.html");
-  $('#footer-container').load("components/footer.html");
-  $('#countdown-container').load("components/countdown.html");
-});
