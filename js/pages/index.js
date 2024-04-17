5e3dd1237a431c3367c057bbce785051eba416b6
Ryan Fuller <145612331+ryanspage@users.noreply.github.com>
2024-04-17 14:15:08 -0500

    Add Big Bend to Weather Location Toggle

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,15 +1,24 @@
 (function($) {
   "use strict"; // Start of use strict
 
-  /* Toggle Weather Display - Show Huntsville & Hide Bloomington */
+  /* Toggle Weather Display - Show Huntsville & Hide Other Locations */
   $("#btn-location-huntsville").on('click', function (event) {
+    $("#weather-big-bend").addClass("d-none");
     $("#weather-bloomington").addClass("d-none");
     setTimeout(() => { $("#weather-huntsville").removeClass("d-none"); }, 100);
   });
 
-  /* Toggle Weather Display - Show Bloomington & Hide Huntsville */
+  /* Toggle Weather Display - Show Big Bend & Hide Other Locations */
+  $("#btn-location-big-bend").on('click', function (event) {
+    $("#weather-huntsville").addClass("d-none");
+    $("#weather-bloomington").addClass("d-none");
+    setTimeout(() => { $("#weather-big-bend").removeClass("d-none"); }, 100);
+  });
+
+  /* Toggle Weather Display - Show Bloomington & Hide Other Locations */
   $("#btn-location-bloomington").on('click', function (event) {
     $("#weather-huntsville").addClass("d-none");
+    $("#weather-big-bend").addClass("d-none");
     setTimeout(() => { $("#weather-bloomington").removeClass("d-none"); }, 100);
   });
 
