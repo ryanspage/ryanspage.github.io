a844cbea5237624d6f7c9ac0f9d84f046d1d8343
Ryan Fuller <145612331+ryanspage@users.noreply.github.com>
2024-07-17 10:57:23 -0500

    Add Yellowstone to Weather Location Toggle

--- js/pages/index.js
+++ js/pages/index.js
@@ -3,23 +3,34 @@
 
   /* Toggle Weather Display - Show Huntsville & Hide Other Locations */
   $("#btn-location-huntsville").on('click', function (event) {
+    $("#weather-bloomington").addClass("d-none");
     $("#weather-big-bend").addClass("d-none");
-    $("#weather-bloomington").addClass("d-none");
+    $("#weather-yellowstone").addClass("d-none");
     setTimeout(() => { $("#weather-huntsville").removeClass("d-none"); }, 100);
-  });
-
-  /* Toggle Weather Display - Show Big Bend & Hide Other Locations */
-  $("#btn-location-big-bend").on('click', function (event) {
-    $("#weather-huntsville").addClass("d-none");
-    $("#weather-bloomington").addClass("d-none");
-    setTimeout(() => { $("#weather-big-bend").removeClass("d-none"); }, 100);
   });
 
   /* Toggle Weather Display - Show Bloomington & Hide Other Locations */
   $("#btn-location-bloomington").on('click', function (event) {
     $("#weather-huntsville").addClass("d-none");
     $("#weather-big-bend").addClass("d-none");
+    $("#weather-yellowstone").addClass("d-none");
     setTimeout(() => { $("#weather-bloomington").removeClass("d-none"); }, 100);
+  });
+
+  /* Toggle Weather Display - Show Big Bend & Hide Other Locations */
+  $("#btn-location-big-bend").on('click', function (event) {
+    $("#weather-huntsville").addClass("d-none");
+    $("#weather-bloomington").addClass("d-none");
+    $("#weather-yellowstone").addClass("d-none");
+    setTimeout(() => { $("#weather-big-bend").removeClass("d-none"); }, 100);
+  });
+
+  /* Toggle Weather Display - Show Yellowstone & Hide Other Locations */
+  $("#btn-location-yellowstone").on('click', function (event) {
+    $("#weather-huntsville").addClass("d-none");
+    $("#weather-bloomington").addClass("d-none");
+    $("#weather-big-bend").addClass("d-none");
+    setTimeout(() => { $("#weather-yellowstone").removeClass("d-none"); }, 100);
   });
 
 })(jQuery); // End of use strict
