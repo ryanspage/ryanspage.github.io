06a7649d07e7c6b3c525efa405f6c972a4dfca7e
Ryan Fuller <145612331+ryanspage@users.noreply.github.com>
2024-08-21 13:13:29 -0500

    Randomize Photo Gallery Order

--- js/pages/photography.js
+++ js/pages/photography.js
@@ -16,6 +16,9 @@
 
   $(document).ready(function(){
 
+    // Randomize gallery image order
+    shuffle(galleryImages);
+
     // Create gallery of images
     for (var i = 0; i < galleryImages.length; i++) {
       $("#gallery").append(
@@ -58,5 +61,22 @@ function showHideViewMore() {
   }
   else {
     $("#row-view-more").show()
+  }
+}
+
+/* Randomizes the order of items in an array */
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
   }
 }
