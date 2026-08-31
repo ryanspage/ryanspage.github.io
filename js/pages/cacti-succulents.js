981a960439b5265f4eda0776cff818012856e5fd
Ryan Fuller <145612331+ryanspage@users.noreply.github.com>
2026-08-31 17:16:55 -0500

    Modify Photo Slideshows to Dynamically Build HTML

--- js/pages/cacti-succulents.js
+++ js/pages/cacti-succulents.js
@@ -9,10 +9,63 @@
     xl: 1200 // Extra Large
   };
 
-  // Randomize Slideshow Order
-  shuffle(document.getElementsByClassName('plant-slide'));
+  // Slideshow file path and images
+  let slideshowPath = '/img/cacti-succulents/';
+  let slideshowImages = [['agave_potatorum.jpg', 'Agave potatorum'],
+                         ['astrophytum_capricorne_1.jpg', 'Astrophytum capricorne'],
+                         ['astrophytum_capricorne_2.jpg', 'Astrophytum capricorne'],
+                         ['echinocereus_davisii_1.jpg', 'Echinocereus davisii'],
+                         ['echinocereus_davisii_2.jpg', 'Echinocereus davisii'],
+                         ['echinocereus_reichenbachii_1.jpg', 'Echinocereus reichenbachii'],
+                         ['echinocereus_reichenbachii_2.jpg', 'Echinocereus reichenbachii'],
+                         ['echinopsis_subdenudata_1.jpg', 'Echinopsis subdenudata'],
+                         ['echinopsis_subdenudata_2.jpg', 'Echinopsis subdenudata'],
+                         ['euphorbia_milii.jpg', 'Euphorbia milii'],
+                         ['gymnocalycium_baldianum.jpg', 'Gymnocalycium baldianum'],
+                         ['gymnocalycium_bruchii_1.jpg', 'Gymnocalycium bruchii'],
+                         ['gymnocalycium_bruchii_2.jpg', 'Gymnocalycium bruchii'],
+                         ['gymnocalycium_friedrichii.jpg', 'Gymnocalycium friedrichii'],
+                         ['gymnocalycium_piricarpum.jpg', 'Gymnocalycium piricarpum'],
+                         ['gymnocalycium_ragonesei.jpg', 'Gymnocalycium ragonesei'],
+                         ['gymnocalycium_saglionis.jpg', 'Gymnocalycium saglionis'],
+                         ['lithops_aucampiae.jpg', 'Lithops aucampiae'],
+                         ['lithops_dorotheae.jpg', 'Lithops dorotheae'],
+                         ['lithops_karasmontana.jpg', 'Lithops karasmontana'],
+                         ['lithops_lesliei.jpg', 'Lithops lesliei'],
+                         ['lithops_pseudotruncatella.jpg', 'Lithops pseudotruncatella'],
+                         ['mammillaria_bocasana.jpg', 'Mammillaria bocasana'],
+                         ['mammillaria_guelzowiana_1.jpg', 'Mammillaria guelzowiana'],
+                         ['mammillaria_guelzowiana_2.jpg', 'Mammillaria guelzowiana'],
+                         ['mammillaria_hahniana_1.jpg', 'Mammillaria hahniana'],
+                         ['mammillaria_hahniana_2.jpg', 'Mammillaria hahniana'],
+                         ['oreocereus_celsianus.jpg', 'Oreocereus celsianus'],
+                         ['parodia_werneri_1.jpg', 'Parodia werneri'],
+                         ['parodia_werneri_2.jpg', 'Parodia werneri'],
+                         ['rebutia_albipilosa_1.jpg', 'Rebutia albipilosa'],
+                         ['rebutia_albipilosa_2.jpg', 'Rebutia albipilosa'],
+                         ['rebutia_krainziana.jpg', 'Rebutia krainziana'],
+                         ['rebutia_narvaecensis.jpg', 'Rebutia narvaecensis'],
+                         ['setiechinopsis_mirabilis_1.jpg', 'Setiechinopsis mirabilis'],
+                         ['setiechinopsis_mirabilis_2.jpg', 'Setiechinopsis mirabilis']]
 
   $(document).ready(function(){
+
+    // Randomize slideshow order
+    shuffle(slideshowImages);
+
+    // Create image slides with captions
+    for (var i = 0; i < slideshowImages.length; i++) {
+      $("#plant-slideshow").append(
+        '<div class=\"plant-slide col px-1\">' +
+          '<div class=\"card\">' +
+          	'<img data-lazy=\"' + slideshowPath + slideshowImages[i][0] + '\" class=\"card-img\">' +
+          	'<div class=\"card-header mt-0 p-1\">' +
+          	  '<p class=\"card-text image-caption font-italic text-body\">' + slideshowImages[i][1] + '</p>' +
+          	'</div>' +
+          '</div>' +
+        '</div>'
+      );
+    }
 
     // Slideshow Configuration
     $('#plant-slideshow').slick({
@@ -56,27 +109,19 @@
 
 })(jQuery); // End of use strict
 
-/* Randomizes the order of a set of HTML elements */
-function shuffle(elems) {
-  allElems = (function(){
-	var ret = [], l = elems.length;
-	while (l--) { ret[ret.length] = elems[l]; }
-	return ret;
-    })();
+/* Randomizes the order of items in an array */
+function shuffle(array) {
+  let currentIndex = array.length;
 
-    var shuffled = (function(){
-        var l = allElems.length, ret = [];
-        while (l--) {
-            var random = Math.floor(Math.random() * allElems.length),
-                randEl = allElems[random].cloneNode(true);
-            allElems.splice(random, 1);
-            ret[ret.length] = randEl;
-        }
-        return ret;
-    })(), l = elems.length;
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
 
-    while (l--) {
-        elems[l].parentNode.insertBefore(shuffled[l], elems[l].nextSibling);
-        elems[l].parentNode.removeChild(elems[l]);
-    }
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
 }
--- js/pages/roman-history.js
+++ js/pages/roman-history.js
@@ -9,10 +9,50 @@
     xl: 1200 // Extra Large
   };
 
-  // Randomize Slideshow Order
-  shuffle(document.getElementsByClassName('coin-slide'));
+  // Slideshow file path and images
+  let slideshowPath = '/img/roman-history/roman-coins/';
+  let slideshowImages = [['litra_1.jpg', 'Roman Republic (269 BC)'],
+                         ['semuncia_1.jpg', 'Roman Republic (215-212 BC)'],
+                         ['furia_1.jpg', 'Roman Republic (84 BC)'],
+                         ['tiberius_1.jpg', 'Tiberius (14-37 AD)'],
+                         ['claudius_3.jpg', 'Claudius (41-54 AD)'],
+                         ['vespasian_1.jpg', 'Vespasian (69-79 AD)'],
+                         ['trajan_2.jpg', 'Trajan (98-117 AD)'],
+                         ['trajan_3.jpg', 'Trajan (98-117 AD)'],
+                         ['trajan_5.jpg', 'Trajan (98-117 AD)'],
+                         ['hadrian_1.jpg', 'Hadrian (117-138 AD)'],
+                         ['septimius_severus_2.jpg', 'Septimius Severus (193-211 AD)'],
+                         ['maximinus_thrax_1.jpg', 'Maximinus Thrax (235-238 AD)'],
+                         ['aurelian_2.jpg', 'Aurelian (270-275 AD)'],
+                         ['aurelian_8.jpg', 'Aurelian (270-275 AD)'],
+                         ['probus_3.jpg', 'Probus (276-282 AD)'],
+                         ['probus_4.jpg', 'Probus (276-282 AD)'],
+                         ['carus_1.jpg', 'Carus (282-283 AD)'],
+                         ['constantine_1_2.jpg', 'Constantine I (306-337 AD)'],
+                         ['constantine_1_3.jpg', 'Constantine I (306-337 AD)'],
+                         ['urbs_roma_2.jpg', 'Urbs Roma (330-346 AD)'],
+                         ['constantius_2_2.jpg', 'Constantius II (337-361 AD)'],
+                         ['valens_3.jpg', 'Valens (364-378 AD)'],
+                         ['arcadius_2.jpg', 'Arcadius (383-408 AD)']]
 
   $(document).ready(function(){
+
+    // Randomize slideshow order
+    shuffle(slideshowImages);
+
+    // Create image slides with captions
+    for (var i = 0; i < slideshowImages.length; i++) {
+      $("#coin-slideshow").append(
+        '<div class=\"coin-slide col px-0\">' +
+          '<div class=\"card\">' +
+            '<img data-lazy=\"' + slideshowPath + slideshowImages[i][0] + '\" class=\"card-img\">' +
+            '<div class=\"card-header mt-0 p-1\">' +
+              '<p class=\"card-text image-caption font-italic text-body\">' + slideshowImages[i][1] + '</p>' +
+            '</div>' +
+          '</div>' +
+        '</div>'
+      );
+    }
 
     // Slideshow Configuration
     $('#coin-slideshow').slick({
@@ -47,27 +87,19 @@
 
 })(jQuery); // End of use strict
 
-/* Randomizes the order of a set of HTML elements */
-function shuffle(elems) {
-  allElems = (function(){
-	var ret = [], l = elems.length;
-	while (l--) { ret[ret.length] = elems[l]; }
-	return ret;
-    })();
+/* Randomizes the order of items in an array */
+function shuffle(array) {
+  let currentIndex = array.length;
 
-    var shuffled = (function(){
-        var l = allElems.length, ret = [];
-        while (l--) {
-            var random = Math.floor(Math.random() * allElems.length),
-                randEl = allElems[random].cloneNode(true);
-            allElems.splice(random, 1);
-            ret[ret.length] = randEl;
-        }
-        return ret;
-    })(), l = elems.length;
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
 
-    while (l--) {
-        elems[l].parentNode.insertBefore(shuffled[l], elems[l].nextSibling);
-        elems[l].parentNode.removeChild(elems[l]);
-    }
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
 }
